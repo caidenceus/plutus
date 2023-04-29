1b64c98f94062a56c319b7884c1a0ccc5585a29c
caidenceus <caidentp@icloud.com>
2023-04-28 19:22:07 -0500

    Add overview animation

--- plutus/src/components/home/OverviewTiles.js
+++ plutus/src/components/home/OverviewTiles.js
@@ -1,11 +1,33 @@
 import Typography from "../common/Typography";
-import { colors } from "../common/colors"
+import { colors } from "../common/colors";
+import "./OverviewTiles.css";
+
+import { useState } from "react";
 
 function Tile({ title, dollarAmount, color }) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  function handleIsHovered() {
+    setIsHovered(!isHovered);
+  }
+
+  const tileStyle = {
+    color: "white",
+    backgroundColor: color,
+    borderRadius: "25px",
+    width: "20%",
+    padding: "20px"
+  };
+
   return (
-    <div style={{color: "white", backgroundColor: color, borderRadius: "25px", width: "22%"}}>
-      <Typography variant="h5">{dollarAmount}</Typography>
-      <Typography variant="h6">{title}</Typography>
+    <div
+      style={tileStyle}
+      onMouseEnter={handleIsHovered}
+      onMouseLeave={handleIsHovered}
+      class={isHovered && "hover-animation"}
+    >
+      <Typography variant="h5" margin={"0"}>${dollarAmount.toLocaleString()}</Typography>
+      <Typography variant="h6" margin={"0"}>{title}</Typography>
     </div>
   );
 }
@@ -13,10 +35,10 @@ function Tile({ title, dollarAmount, color }) {
 function OverviewTiles() {
   return (
     <div style={{display: "flex", justifyContent: "space-between", padding: "0 50px"}}>
-      <Tile title={"test"} dollarAmount={1000} color={colors.red} />
-      <Tile title={"test"} dollarAmount={1000} color={colors.yellow} />
-      <Tile title={"test"} dollarAmount={1000} color={colors.green} />
-      <Tile title={"test"} dollarAmount={1000} color={colors.teal} />
+      <Tile title={"Total"} dollarAmount={1000.00} color={colors.red} />
+      <Tile title={"Savings"} dollarAmount={1000.00} color={colors.yellow} />
+      <Tile title={"Investments"} dollarAmount={1000.00} color={colors.green} />
+      <Tile title={"Cost of living"} dollarAmount={1000.00} color={colors.teal} />
     </div>
   );
 }
